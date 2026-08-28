48bc1c84461b3b8348b7aa7e3804a446c28a1b84
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-08-28 21:59:42 +0700

    .

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $appSettings->app_name ?? config('app.name', 'Laravel') }}</title>
-        <link rel="icon" type="image/png" href="{{ ! empty($appSettings->app_logo_path) ? asset('storage/'.$appSettings->app_logo_path) : asset('favicon.ico') }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
